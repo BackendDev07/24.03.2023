5be35b4107a79c5784579d3cce1dd20f71d357e9
BackendDev07 <browhith3.uz@gmail.com>
2023-03-29 16:20:21 +0500

    To do list

--- main.js
+++ main.js
@@ -49,3 +49,12 @@ function newElement() {
     }
   }
 }
+
+
+var input = document.getElementById("myInput");
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("myBtn").click();
+  }
+});
